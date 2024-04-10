fa33d1b6cf94d1dc71f23c6c80614bbaacaf2aea
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-10 02:18:06 -0400

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,7 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        boolean isPositive = (number & 1) == 0;
-        System.out.println(isPositive);
+        System.out.println(number > 0);
     }
 }
