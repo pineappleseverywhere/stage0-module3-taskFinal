30d7a13f61910a9958717abf96c0dfc8a3d49f87
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-10 02:15:08 -0400

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int thousandsDigit = number / 1000;
+        int hundredsDigit = (number % 1000)/ 100;
+        int tensDigit = (number % 100) / 10;
+        int unitsDigit = number % 10;
+        System.out.println(thousandsDigit + hundredsDigit + tensDigit + unitsDigit);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        System.out.println(reversedNumber);
     }
 }
